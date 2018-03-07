6d12fc4c83d0dcfa2ce4f762eda9a6bd2fcdba6b
Oleksandr Zabudskyi <zabudskyioleksandr@gmail.com>
2018-03-07 23:52:24 +0200

    Refactoring

--- src/ua/tasks/task2/Model.java
+++ src/ua/tasks/task2/Model.java
@@ -7,14 +7,7 @@ public class Service {
     private int minBorder;
     private int maxBorder;
     private int randomValue;
-    private List<String> statistics;
-
-    public Service() {
-        this.minBorder = 0;
-        this.maxBorder = 100;
-        this.randomValue = (int) (Math.random() * 101);
-        this.statistics = new ArrayList<>();
-    }
+    private List<String> statistics = new ArrayList<>();
 
     public int getMinBorder() {
         return minBorder;
@@ -24,11 +17,16 @@ public class Service {
         return maxBorder;
     }
 
+    public int getRandomValue() {
+        return randomValue;
+    }
+
     public List<String> getStatistics() {
         return statistics;
     }
 
     public boolean isGuessValueFromRange(int value) {
+        statistics.add(Integer.toString(value));
         int result = value - randomValue;
 
         if (result == 0) {
@@ -36,18 +34,21 @@ public class Service {
         }
 
         if (result < 0) {
-            minBorder = value + 1;
+            minBorder = value;
         } else {
-            maxBorder = value - 1;
+            maxBorder = value;
         }
         return false;
 
     }
 
-    public void generateRandomValue(int min, int max) {
-        minBorder = min;
-        maxBorder = max;
-        int margin = maxBorder - minBorder;
-        randomValue = (int) (Math.random() * (margin + 1) + minBorder);
+    public void setBorders(int minBorder, int maxBorder) {
+        this.minBorder = minBorder;
+        this.maxBorder = maxBorder;
+    }
+
+    public void generateRandomValue() {
+        randomValue = (int) Math.ceil(Math.random() *
+                (maxBorder - minBorder - 1) + minBorder);
     }
 }
