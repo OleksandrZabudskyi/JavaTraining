049bc65feaa98cabd34666ec3d7a2be270a2c950
OleksandrZabudskyi <alexjavaw@gmail.com>
2018-03-10 00:13:44 +0200

    Add resource bundle

--- src/ua/tasks/task2/View.java
+++ src/ua/tasks/task2/View.java
@@ -1,98 +1,104 @@
 package ua.tasks.task2;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class View {
-    public static final String EQUAL_SING = "=";
     public static final String SPACE_SING = " ";
     public static final String SEMICOLON = ";";
     public static final String OPENS_BRACKET = "(";
     public static final String CLOSING_BRACKET = ")";
 
-    public static final String SELECT_TYPE_OF_RANGE = "By default program uses range: (0;100)" +
-            "\n" + "If you wish to apply custom rage" +
-            "\n" + "please input \"yes\" or \"not\" otherwise";
-    public static final String WRONG_INPUT_SELECTED_TYPE = "You have entered incorrect option, please input" +
-            " \"yes\" to confirm or \"not\" otherwise";
+    // Resource Bundle Installation's
+    public static final String MESSAGES_BUNDLE_NAME = "ua.tasks.task2.resources.messages";
+    public static final ResourceBundle bundle =
+            ResourceBundle.getBundle(
+                    MESSAGES_BUNDLE_NAME,
+                    //new Locale("ua"));  // Ukrainian
+                    new Locale("en"));        // English
 
-    public static final String CUSTOM_BORDER_RANGE_MESSAGE = "Input range with pace at least = 2";
-    public static final String WRONG_RANGE_PACE_MESSAGE = "You have wrong pace: ";
-    public static final String INPUT_MIN_BORDER_MESSAGE = "Input min border";
-    public static final String INPUT_MAX_BORDER_MESSAGE = "Input max border";
-    public static final String INPUT_INT_MESSAGE = "Input integer value:";
-    public static final String WRONG_INPUT_MESSAGE = "Wrong input! Repeat please.";
-    public static final String DEFINED_RANGE_MESSAGE = "Your range:";
-    public static final String WRONG_INPUT_RANGE_MESSAGE = "You entered wrong range:";
-    public static final String WRONG_RANGE_MESSAGE = "Wrong range! Repeat please, and select from correct range: ";
-    public static final String UNSUCCESSFUL_ATTEMPT_MESSAGE = "Your attempt is unsuccessful. Repeat please";
-    public static final String COINCIDENCE_MESSAGE = "Congratulation! Your guessed number is: ";
-    public static final String INPUT_STATISTICS_MESSAGE = "Input statistics: ";
+    public static final String SELECT_TYPE_OF_RANGE = "input.range.type";
+    public static final String WRONG_INPUT_SELECTED_TYPE = "input.wrong.type";
+    public static final String CUSTOM_BORDER_RANGE = "input.custom.range";
+    public static final String WRONG_RANGE_PACE = "input.wrong.pace";
+    public static final String INPUT_MIN_BORDER_DATA = "input.min.border.data";
+    public static final String INPUT_MAX_BORDER_DATA = "input.max.border.data";
+    public static final String INPUT_INT_DATA = "input.int.data";
+    public static final String WRONG_INPUT_DATA = "input.wrong.data";
+    public static final String DEFINED_RANGE = "input.defined.range";
+    public static final String WRONG_INPUT_RANGE = "input.wrong.range.data";
+    public static final String WRONG_RANGE = "input.wrong.range";
+    public static final String UNSUCCESSFUL_ATTEMPT = "input.wrong.attempt";
+    public static final String CONGRATULATION = "input.congratulation";
+    public static final String INPUT_STATISTICS = "input.statistics";
 
     public void printMessage(String... message) {
         System.out.println(concatenationString(message));
     }
 
-    private String concatenationString(String... message){
+    private String concatenationString(String... message) {
         StringBuilder concatString = new StringBuilder();
-        for(String v : message) {
+        for (String v : message) {
             concatString = concatString.append(v);
         }
         return new String(concatString);
     }
 
     public void printRangeMessage(Model model) {
-        printMessage(DEFINED_RANGE_MESSAGE, rangeToString(model.getMinBorder(), model.getMaxBorder()));
+        printMessage(bundle.getString(DEFINED_RANGE), rangeToString(model.getMinBorder(), model.getMaxBorder()));
     }
 
     public void printWrongRangeInput(Model model) {
-        printMessage(WRONG_RANGE_MESSAGE, rangeToString(model.getMinBorder(), model.getMaxBorder()));
+        printMessage(bundle.getString(WRONG_RANGE), rangeToString(model.getMinBorder(), model.getMaxBorder()));
     }
 
     public void printCongratulationMessage(Model model) {
-        printMessage(COINCIDENCE_MESSAGE, Integer.toString(model.getRandomValue()));
+        printMessage(bundle.getString(CONGRATULATION), Integer.toString(model.getRandomValue()));
     }
 
-    public String rangeToString(int minBorder, int maxBorder) {
+    private String rangeToString(int minBorder, int maxBorder) {
         return concatenationString(OPENS_BRACKET, Integer.toString(minBorder),
                 SEMICOLON, Integer.toString(maxBorder), CLOSING_BRACKET);
     }
 
     public void printStatistics(Model model) {
-        printMessage(INPUT_STATISTICS_MESSAGE, model.getStatistics().toString());
+        printMessage(bundle.getString(INPUT_STATISTICS), model.getStatistics().toString());
     }
 
     public void printWrongIntInput() {
-        printMessage(WRONG_INPUT_MESSAGE, SPACE_SING, INPUT_INT_MESSAGE);
+        printMessage(bundle.getString(WRONG_INPUT_DATA), SPACE_SING, bundle.getString(INPUT_INT_DATA));
     }
 
     public void printWrongAttemptMessage() {
-        printMessage(UNSUCCESSFUL_ATTEMPT_MESSAGE);
+        printMessage(bundle.getString(UNSUCCESSFUL_ATTEMPT));
     }
 
     public void printIntInputMessage() {
-        printMessage(INPUT_INT_MESSAGE);
+        printMessage(bundle.getString(INPUT_INT_DATA));
     }
 
     public void printRangeTypeMessage() {
-        printMessage(SELECT_TYPE_OF_RANGE);
+        printMessage(bundle.getString(SELECT_TYPE_OF_RANGE));
     }
 
     public void printWrongInputTypeMessage() {
-        printMessage(WRONG_INPUT_SELECTED_TYPE);
+        printMessage(bundle.getString(WRONG_INPUT_SELECTED_TYPE));
     }
 
     public void printCustomRangeMessage() {
-        printMessage(CUSTOM_BORDER_RANGE_MESSAGE);
+        printMessage(bundle.getString(CUSTOM_BORDER_RANGE));
     }
 
     public void printInputMinBorder() {
-        printMessage(INPUT_MIN_BORDER_MESSAGE);
+        printMessage(bundle.getString(INPUT_MIN_BORDER_DATA));
     }
 
     public void printInputMaxBorder() {
-        printMessage(INPUT_MAX_BORDER_MESSAGE);
+        printMessage(INPUT_MAX_BORDER_DATA);
     }
 
     public void printWrongRangePaceMessage(int pace, int min, int max) {
-        printMessage(View.WRONG_RANGE_PACE_MESSAGE, Integer.toString(pace),
-                SPACE_SING, WRONG_INPUT_RANGE_MESSAGE, rangeToString(min, max));
+        printMessage(View.WRONG_RANGE_PACE, Integer.toString(pace),
+                SPACE_SING, WRONG_INPUT_RANGE, rangeToString(min, max));
     }
 }
