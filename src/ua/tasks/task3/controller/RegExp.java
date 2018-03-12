ac81eb6ef7c1a3fbc1fd64f2e90a5b31a106d43e
Oleksandr Zabudskyi <zabudskyioleksandr@gmail.com>
2018-03-12 11:46:37 +0200

    refactoring

--- src/ua/tasks/task3/controller/RegExp.java
+++ src/ua/tasks/task3/controller/RegExp.java
@@ -8,16 +8,16 @@ import ua.tasks.task3.model.Group;
  * @autor Zabudskyi Oleksandr zabudskyioleksandr@gmail.com
  */
 public interface RegExp {
-    String NAME = "^[A-ZА-ЯІЇ][a-zA-ZА-Яа-яІiЇїʼ]*$";
+    String NAME = "^[A-ZА-ЩЮЯҐІЇЄ][a-zA-ZА-ЩЬЮЯҐІЇЄа-щьюяґіїєʼ]*$";
     String NICKNAME = "^[a-z][a-z0-9]*$";
-    String COMMENT = "^[a-zA-ZА-Яа-яІiЇїʼ]+$";
+    String COMMENT = "^[a-zA-ZА-ЩЬЮЯҐІЇЄа-щьюяґіїєʼ]+$";
     String MAIL = "^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}$";
     String PHONE = "^\\+\\d{2}\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}$";
     String SKYPE = "^[a-zA-Z][a-zA-Z0-9\\.,\\-_]{5,31}$";
     String GROUP = "(" + Group.FAMILY + "|" + Group.FRIENDS + "|" + Group.WORK + "|" + Group.RELATIVES + ")";
     String INDEX = "^[0-9]{5}$";
-    String CITY = "^[A-ZА-ЯІЇ][a-zA-ZА-Яа-яІiЇїʼ]+$";
-    String STREET = "^[A-ZА-ЯІЇ][a-zA-ZА-Яа-яІiЇїʼ]+$";
+    String CITY = "^[A-ZА-ЩЮЯҐІЇЄ][a-zA-ZА-ЩЬЮЯҐІЇЄа-щьюяґіїєʼ]+$";
+    String STREET = "^[A-ZА-ЩЮЯҐІЇЄ][a-zA-ZА-ЩЬЮЯҐІЇЄа-щьюяґіїєʼ]+$";
     String HOUSE_NUMBER = "^[0-9]+(?:[/0-9])*$";
     String APARTMENT_NUMBER = "^[0-9]+$";
 }
