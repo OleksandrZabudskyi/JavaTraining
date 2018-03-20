9dbff7ee380823a965443765b39dbf8e4d555e7e
OleksandrZabudskyi <alexjavaw@gmail.com>
2018-03-20 23:39:44 +0200

    refactoring

--- src/ua/tasks/WebRegistForm(block06)/src/main/java/ua/training/model/dao/DBConnection.java
+++ src/ua/tasks/WebRegistForm(block06)/src/main/java/ua/training/model/dao/DBConnection.java
@@ -1,12 +1,8 @@
 package ua.training.model.dao;
 
-import com.mysql.fabric.jdbc.FabricMySQLDriver;
-
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
