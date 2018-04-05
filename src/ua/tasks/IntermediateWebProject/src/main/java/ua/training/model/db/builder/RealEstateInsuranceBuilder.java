7cd2ce68649c4ce8c21c2ebd7d767ff0919365c1
OleksandrZabudskyi <alexjavaw@gmail.com>
2018-04-06 02:12:08 +0300

    implement builder

---
+++ src/ua/tasks/IntermediateWebProject/src/main/java/ua/training/model/db/builder/RealEstateInsuranceBuilder.java
@@ -0,0 +1,42 @@
+package ua.training.model.db.builder;
+
+import ua.training.model.bean.RealEstateInsurance;
+
+import java.math.BigDecimal;
+
+public class RealEstateInsuranceBuilder {
+    private Long id;
+    private BigDecimal liabilityPrice;
+    private Double risk;
+    private String name;
+    private BigDecimal valuationOfPropertyPrice;
+
+    public RealEstateInsuranceBuilder setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public RealEstateInsuranceBuilder setLiabilityPrice(BigDecimal liabilityPrice) {
+        this.liabilityPrice = liabilityPrice;
+        return this;
+    }
+
+    public RealEstateInsuranceBuilder setRisk(Double risk) {
+        this.risk = risk;
+        return this;
+    }
+
+    public RealEstateInsuranceBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public RealEstateInsuranceBuilder setValuationOfPropertyPrice(BigDecimal valuationOfPropertyPrice) {
+        this.valuationOfPropertyPrice = valuationOfPropertyPrice;
+        return this;
+    }
+
+    public RealEstateInsurance createRealEstateInsurance() {
+        return new RealEstateInsurance(id, liabilityPrice, risk, name, valuationOfPropertyPrice);
+    }
+}
