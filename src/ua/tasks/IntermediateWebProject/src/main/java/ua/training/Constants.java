b341d7eeba703bd1f6fa633cec8763f7191e7c76
OleksandrZabudskyi <alexjavaw@gmail.com>
2018-04-06 01:36:53 +0300

    add new constants

--- src/ua/tasks/IntermediateWebProject/src/main/java/ua/training/Constants.java
+++ src/ua/tasks/IntermediateWebProject/src/main/java/ua/training/Constants.java
@@ -1,16 +1,24 @@
 package ua.training;
 
 public interface Constants {
-    String WELCOME_PAGE = "/jsp/welcome.jsp";
-    String INDEX_PAGE = "/jsp/index.jspx";
+    String WELCOME_PAGE = "/jsp/main.jsp";
+    String INDEX_PAGE = "/jsp/index.jsp";
     String ERROR_PAGE = "/jsp/error.jspx";
+    String TABLE_NAME = "tableName";
+    String LIABILITY_INSURANCES = "Liability Insurances";
     String DERIVATIVE = "Derivative";
+    String LIABILITIES = "liabilities";
+    String DERIVATIVE_PRICE = "derivativePrice";
+    String PRICE = "price";
+    String RISK = "risk";
+    String ACT = "act";
     String ACTIVE = "active";
     String TRUE = "TRUE";
     String ERROR_MESSAGE = "errorMessage";
     String MESSAGE = "message";
-    String INVALID_MESSAGE = "Input data is not equal to pattern ";
+    String INVALID_MESSAGE = "Incorrect input data should be decimal or floating-point ";
     String DERIVATIVE_CREATED = "New derivative created";
+    String SORTED_BY_RISK = "Liabilities in derivative are sorted by risk";
     String FIND = "find";
     String NOTHING_FOUND = "Nothing found";
 
