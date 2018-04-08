e676e0ec50a7aac8749f2a878b7156fe343d23ef
Oleksandr Zabudskyi <zabudskyioleksandr@gmail.com>
2018-04-08 14:02:08 +0300

    refactor constants

---
+++ src/ua/tasks/IntermediateWebProject/src/main/java/ua/training/util/MassageConstant.java
@@ -0,0 +1,14 @@
+package ua.training.util;
+
+public interface MassageConstant {
+    String ACTIVE = "active";
+    String TRUE = "TRUE";
+    String ERROR_MESSAGE = "errorMessage";
+    String MESSAGE = "message";
+    String INVALID_MESSAGE = "Incorrect input data should be decimal or floating-point ";
+    String DERIVATIVE_CREATED = "New derivative created";
+    String SORTED_BY_RISK = "Liabilities in derivative are sorted by risk";
+    String FIND = "find";
+    String NOTHING_FOUND = "Nothing found";
+
+}
