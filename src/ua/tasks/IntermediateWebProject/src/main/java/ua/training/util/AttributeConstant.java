5dc3349c3842ec3356b6d37c2140a89f16e640ee
OleksandrZabudskyi <alexjavaw@gmail.com>
2018-04-08 14:02:08 +0300

    refactor constants

---
+++ src/ua/tasks/IntermediateWebProject/src/main/java/ua/training/util/AttributeConstant.java
@@ -0,0 +1,12 @@
+package ua.training.util;
+
+public interface AttributeConstant {
+    String TABLE_NAME = "tableName";
+    String LIABILITY_INSURANCES = "Liability Insurances";
+    String DERIVATIVE = "Derivative";
+    String LIABILITIES = "liabilities";
+    String DERIVATIVE_PRICE = "derivativePrice";
+    String PRICE = "price";
+    String ACT = "act";
+    String ID = "id";
+}
---
+++ src/ua/tasks/IntermediateWebProject/src/main/java/ua/training/util/PageConstant.java
@@ -0,0 +1,7 @@
+package ua.training.util;
+
+public interface PageConstant {
+    String INDEX_PAGE = "/jsp/index.jsp";
+    String WELCOME_PAGE = "/jsp/main.jsp";
+    String ERROR_PAGE = "/jsp/error.jspx";
+}
