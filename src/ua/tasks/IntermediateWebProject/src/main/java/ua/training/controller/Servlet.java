90661193f59135c2461fd3f265b1f45c8d587e3b
OleksandrZabudskyi <alexjavaw@gmail.com>
2018-04-10 19:41:42 +0300

    add error handling config to web.xml

--- src/ua/tasks/IntermediateWebProject/src/main/java/ua/training/controller/Servlet.java
+++ src/ua/tasks/IntermediateWebProject/src/main/java/ua/training/controller/Servlet.java
@@ -24,16 +24,8 @@ public class Servlet extends HttpServlet {
     private void processRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
             throws ServletException, IOException {
         String page;
-
-        try {
-            Command command = commandExtractor.getCommand(httpServletRequest);
-            page = command.execute(httpServletRequest, httpServletResponse);
-        } catch (Exception e) {
-            //TODO Add to log file
-            e.printStackTrace();
-            httpServletRequest.setAttribute(ERROR_MESSAGE, e.getMessage());
-            page = PageConstant.ERROR_PAGE;
-        }
+        Command command = commandExtractor.getCommand(httpServletRequest);
+        page = command.execute(httpServletRequest, httpServletResponse);
         RequestDispatcher dispatcher = httpServletRequest.getRequestDispatcher(page);
         dispatcher.forward(httpServletRequest, httpServletResponse);
     }
