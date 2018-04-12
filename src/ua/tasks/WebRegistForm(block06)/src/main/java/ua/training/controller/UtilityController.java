87aa54af1327d2ec8e6a4fef21eeb193b8b50e4f
OleksandrZabudskyi <alexjavaw@gmail.com>
2018-04-13 00:01:03 +0300

    removing filter and adding util class

---
+++ src/ua/tasks/WebRegistForm(block06)/src/main/java/ua/training/controller/UtilityController.java
@@ -0,0 +1,17 @@
+package ua.training.controller;
+
+import static ua.training.util.Regex.REGEX_EMAIL;
+import static ua.training.util.Regex.REGEX_NAME_LAT;
+
+public class UtilityController {
+
+    public boolean isDataValid(String... parameters) {
+        boolean state = false;
+        for (String parameter: parameters) {
+            if (parameter != null && (parameter.matches(REGEX_NAME_LAT) || parameter.matches(REGEX_EMAIL))) {
+                state = true;
+            }
+        }
+        return state;
+    }
+}
